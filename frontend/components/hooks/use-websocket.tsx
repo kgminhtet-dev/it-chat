87459b7e64b5cfb6be705e87f9d9d83a69c47709
kgminhtet <kgminhtet.dev@gmail.com>
2024-09-02 15:10:41 +0630

    change web socket URL

--- frontend/components/hooks/use-websocket.tsx
+++ frontend/components/hooks/use-websocket.tsx
@@ -9,7 +9,7 @@ export default function useWebSocket(token: string) {
   const { toast } = useToast();
   const setSocket = useAppStore((state) => state.setSocket);
   const [isConnected, setIsConnected] = useState(false);
-  const url = `ws://localhost:8080`;
+  const url = `ws://it-chat.onrender.com`;
 
   useEffect(() => {
     const socket = io(url, {
